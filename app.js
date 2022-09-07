b8403511744d29b3c41cd69ecf21a2e55b8e1cec
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-07 21:48:41 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path'); // 파일, 디렉터리 정보 관리 
+const morgan = require('morgan'); //요청과 응답에 대한 정보 콘솔 기록
+
+const app = express();
+const port = process.env.port || 80;
+
+app.use(express.static("public_html"));
+app.listen(port, function () {
+    console.log("서버 시작됨");
+});

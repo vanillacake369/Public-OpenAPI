85b2288fa1a368a9891fa340e2e04c449fb80bad
Laptop-Limjihoon <lonelynight1026@gmail.com>
2022-09-09 17:28:04 +0900

    naver map HTML에 연결해보기

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path'); // 파일, 디렉터리 정보 관리
 const morgan = require('morgan'); //요청과 응답에 대한 정보 콘솔 기록
 
 const app = express();
-const port = process.env.port || 80;
+const port = process.env.port || 3006;
 
 app.use(express.static("public_html"));
 app.listen(port, function () {
